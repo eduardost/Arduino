534e6ad65ed8e3cd82cf40dbaab7b922941c5bb5
eduardost <eduardostipa@gmail.com>
2025-06-23 22:56:59 -0300

    OledSize fixed code with SH110X.h library

--- OledSize/OledSize.ino
+++ OledSize/OledSize.ino
@@ -3,8 +3,19 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-#define OLED_RESET 4
-Adafruit_SH110X display (OLED_RESET);
+//#define OLED_RESET 4
+//Adafruit_SH110X display (OLED_RESET);
+
+/* Uncomment the initialize the I2C address , uncomment only one, If you get a totally blank screen try the other*/
+#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+                         // e.g. the one with GM12864-77 written on it
+//#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 void setup()
 {
@@ -13,16 +24,16 @@ void setup()
   display.display();
   delay(2000);
   display.setTextSize(1);
-  display.setTextColor(WHITE);
+  display.setTextColor(SH110X_WHITE);
   display.setCursor(0,0);
   display.println("HOLA MUNDO");
-  display.setTextColor(BLACK, WHITE);   //'inverted' text
+  display.setTextColor(SH110X_BLACK, SH110X_WHITE);   //'inverted' text
   display.println("HOLA MUNDO");
   display.setTextSize(2);
-  display.setColor(WHITE);
+  display.setColor(SH110X_WHITE);
   display.println("EDITRONIKX");
   display.setTextSize(2);
-  display.setTextColor(BLACK, WHITE);   //'inverted' text
+  display.setTextColor(SH110X_BLACK, SH110X_WHITE);   //'inverted' text
   display.println("EDITRONIKX");
   display.display();
   delay(4000);
@@ -33,11 +44,11 @@ void loop()
   for (int i=0;i<11<i++){
     display.clearDisplay();
     display.setTextSize(2);
-    display.setTextColor(WHITE);
+    display.setTextColor(SH110X_WHITE);
     display.setCursor(29,0);
     display.println("CONTADOR");
     display.setTextSize(4);
-    display.setTextColor(WHITE);
+    display.setTextColor(SH110X_WHITE);
     display.setCursor(55,20);
     display.println(i);
     display.display();
